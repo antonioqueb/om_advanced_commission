3aed40b674dae4dd6785e638fa863d2f16166734
Antonio Queb <cardstillo@gmail.com>
2026-08-27 22:54:22 -0600

    Actualizar migración para incluir al usuario 66 en la exclusión de comisiones

--- migrations/19.0.5.3.0/post-migrate.py
+++ migrations/19.0.5.3.0/post-migrate.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 def migrate(cr, version):
-    """Excepción acordada con el cliente (27 ago 2026): los usuarios 75 y 65
+    """Excepción acordada con el cliente (27 ago 2026): los usuarios 75, 65 y 66
     venden pero NUNCA comisionan. Solo hacia adelante: no se tocan
     movimientos ya devengados. Editable después en el contacto/usuario."""
     cr.execute("""
         UPDATE res_partner
            SET commission_excluded = TRUE
-         WHERE id IN (SELECT partner_id FROM res_users WHERE id IN (65, 75))
+         WHERE id IN (SELECT partner_id FROM res_users WHERE id IN (65, 75, 66))
     """)
